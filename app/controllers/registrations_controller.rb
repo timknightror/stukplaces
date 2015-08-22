d40e92227d36e0aaca77880df5057d456e75c512
timknightror <tim@52brookedge.com>
2015-08-22 06:06:06 +0000

    added flash messages

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,11 +3,11 @@ class RegistrationsController < Devise::RegistrationsController
   private
   
   def sign_up_params
-    params.require(:user).premit(:name, :email, :password)
+    params.require(:user).permit(:name, :email, :password)
   end
   
   def account_update_params
-    params.require(:user).premit(:name, :email, :password, :current_password)
+    params.require(:user).permit(:name, :email, :password, :current_password)
   end
   
 end
