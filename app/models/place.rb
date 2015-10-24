a69d867b40dc13757f7910370d946bb9b0aead60
timknightror <tim@52brookedge.com>
2015-10-24 21:17:52 +0000

    Added Search - Full Text Only

--- app/models/place.rb
+++ app/models/place.rb
@@ -8,12 +8,22 @@ class Place < ActiveRecord::Base
   geocoded_by :address
   after_validation :geocode
   
+  
+  
   has_many :reviews, dependent: :destroy
   
   def average_rating
     self.reviews.sum(:score) / reviews.size
   rescue ZeroDivisionError
     0
+  end
+  
+  def self.search(search)
+    if search
+      where(['name LIKE ? OR address LIKE ?', "#{search}", "#{search}"])
+    else
+      all
+    end
   end
   
 end
